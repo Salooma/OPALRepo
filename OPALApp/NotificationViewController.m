c9d06b64bdaa9b635e7b82b48f277ad4a1e76186
ajfigueroa <alexjfigueroa@gmail.com>
2014-01-22 21:33:00 -0500

    Added base implementations for storyboard view controllers

---
+++ OPALApp/NotificationViewController.m
@@ -0,0 +1,38 @@
+//
+//  NotificationViewController.m
+//  OPALApp
+//
+//  Created by Alexander Figueroa on 1/22/2014.
+//  Copyright (c) 2014 Alexander Figueroa. All rights reserved.
+//
+
+#import "NotificationViewController.h"
+
+@interface NotificationViewController ()
+
+@end
+
+@implementation NotificationViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ OPALApp/WayfinderViewController.m
@@ -0,0 +1,38 @@
+//
+//  WayfinderViewController.m
+//  OPALApp
+//
+//  Created by Alexander Figueroa on 1/22/2014.
+//  Copyright (c) 2014 Alexander Figueroa. All rights reserved.
+//
+
+#import "WayfinderViewController.h"
+
+@interface WayfinderViewController ()
+
+@end
+
+@implementation WayfinderViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
