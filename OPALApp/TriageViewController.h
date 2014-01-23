3a302bb0593b9b50310b03ec63db4d102d31e2ae
ajfigueroa <alexjfigueroa@gmail.com>
2014-01-23 18:41:27 -0500

    Added and hooked up traigeviewcontroller to the storyboard triage view

---
+++ OPALApp/TriageViewController.h
@@ -0,0 +1,13 @@
+//
+//  TriageViewController.h
+//  OPALApp
+//
+//  Created by Alexander Figueroa on 1/23/2014.
+//  Copyright (c) 2014 Alexander Figueroa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TriageViewController : UIViewController
+
+@end
---
+++ OPALApp/TriageViewController.m
@@ -0,0 +1,17 @@
+//
+//  TriageViewController.m
+//  OPALApp
+//
+//  Created by Alexander Figueroa on 1/23/2014.
+//  Copyright (c) 2014 Alexander Figueroa. All rights reserved.
+//
+
+#import "TriageViewController.h"
+
+@interface TriageViewController ()
+
+@end
+
+@implementation TriageViewController
+
+@end
